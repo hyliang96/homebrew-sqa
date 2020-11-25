03b3265e173e2ccbf4f88e3ce85579b906b70d0f
hyliang96 <hyliang96@hotmail.com>
2020-11-26 00:15:32 +0800

    update sha256

--- Casks/slowquitapps.rb
+++ Casks/slowquitapps.rb
@@ -1,6 +1,6 @@
 cask 'slowquitapps' do
   version '0.8.1'
-  sha256 '418ab46042087b52b656ee3b18854ec0a21593a2da2b341e3090715f035c18a2'
+  sha256 '854425f2e8789d6ce67cf34fa5b015af74cee551355f471d32978fcf1d544ece'
 
   url "https://github.com/hyliang96/SlowQuitApps/releases/download/v#{version}/SlowQuitApps.zip"
   # url "https://github.com/dteoh/SlowQuitApps/releases/download/v#{version}/SlowQuitApps.zip"
@@ -9,12 +9,11 @@ cask 'slowquitapps' do
   appcast 'https://github.com/hyliang96/SlowQuitApps/releases.atom'
 
   depends_on macos: '>= :mojave'
-
+  
   def install
     bin.install 'sqa'
-  end
-
-  app 'SlowQuitApps.app'
+    app 'SlowQuitApps.app'
+  end 
 
   caveats <<-DESC
   Accessibility permissions must be reset after updating or re-installing.
