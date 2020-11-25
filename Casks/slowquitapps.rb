d56e6c4ea3222da7ed2e9dc1b6253f1b29ab3cfb
hyliang96 <hyliang96@hotmail.com>
2020-11-26 01:02:19 +0800

    update sha256

--- Casks/slowquitapps.rb
+++ Casks/slowquitapps.rb
@@ -1,6 +1,6 @@
 cask 'slowquitapps' do
   version '0.8.1'
-  sha256 '854425f2e8789d6ce67cf34fa5b015af74cee551355f471d32978fcf1d544ece'
+  sha256 '7d91ab479b836a2ebe4cf9463c9fb0c6a39a984256ce4933501936fcb157eb03'
 
   url "https://github.com/hyliang96/SlowQuitApps/releases/download/v#{version}/SlowQuitApps.zip"
   # url "https://github.com/dteoh/SlowQuitApps/releases/download/v#{version}/SlowQuitApps.zip"
